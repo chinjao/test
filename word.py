0ee8118a1b9db789ad59461b63acf1be6681200c
chinjao <yuu09ma21@gmail.com>
2015-07-28 18:47:27 +0900

    new

--- word.py
+++ word.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 def string_word(word):
     return word == word[::-1]
 
-print "斁E��を�E力してください"
+print "文章を入力してください"
 str = raw_input()
 i = string_word(str)
 if i == 1:
-    print "回文でぁE
+    print "回文です"
 else :
     print "回文ではありません"
